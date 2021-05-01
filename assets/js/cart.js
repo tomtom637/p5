d7b3a1b8843197d5db3ab6af8de8ab5d73a25466
tomtom637 <tomtom637@gmail.com>
2021-05-01 15:45:14 +0200

    updated cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,67 +1,70 @@
-const cartNum = document.getElementById('cart-num');
+class Cart {
+  constructor() {
+    this.counterElement = document.getElementById('cart-num');
+    this.initialize();
+  }
 
-const getCartLength = () => JSON.parse(localStorage.getItem('cart')).length;
+  // UPDATE THE CART COUNTER VIEW
+  updateCounter() {
+    this.counterElement.textContent = JSON.parse(localStorage.getItem('cart')).length;
+  }
 
-//! INITIALIZATION
-function initializeCart() {
-  if(localStorage.getItem('cart')) return;
+  // INITIALIZATION OF THE CART
+  initialize() {
+    if(localStorage.getItem('cart')) return;
 
-  localStorage.setItem(
-    'cart',
-    JSON.stringify([])
-  );
+    localStorage.setItem(
+      'cart',
+      JSON.stringify([])
+    );
+    this.updateCounter();
+  }
 
-  cartNum.textContent = getCartLength();
+  // ADD ITEM TO CART
+  addItem(item) {
+    const originalContent = JSON.parse(localStorage.getItem('cart'));
+
+    localStorage.setItem(
+      'cart',
+      JSON.stringify([...originalContent, item])
+    );
+    this.updateCounter();
+  }
+
+  // REMOVING ALL OF A KIND FROM CART
+  removeItemType(item) {
+    const originalContent = JSON.parse(localStorage.getItem('cart'));
+
+    const updatedContent = originalContent.filter(items => items !== item);
+
+    localStorage.setItem(
+      'cart',
+      JSON.stringify(updatedContent)
+    );
+    this.updateCounter();
+  }
+
+  // REMOVING ONE OF A KIND FROM CART
+  removeOne(item) {
+    const cartContent = JSON.parse(localStorage.getItem('cart'));
+
+    cartContent.splice(cartContent.indexOf(item), 1);
+
+    localStorage.setItem(
+      'cart',
+      JSON.stringify(cartContent)
+    );
+    this.updateCounter();
+  }
+
+  // EMPTYING THE CART
+  emptyOut() {
+    localStorage.setItem(
+      'cart',
+      JSON.stringify([])
+    );
+    this.updateCounter();
+  }
 }
 
-initializeCart();
-
-//! ADDING TO CART
-function addToCart(item) {
-  const originalContent = JSON.parse(localStorage.getItem('cart'));
-
-  localStorage.setItem(
-    'cart',
-    JSON.stringify([...originalContent, item])
-  );
-
-  cartNum.textContent = getCartLength();
-}
-
-//! REMOVING ALL OF A KIND FROM CART
-function removeFromCart(item) {
-  const originalContent = JSON.parse(localStorage.getItem('cart'));
-
-  const updatedContent = originalContent.filter(items => items !== item);
-
-  localStorage.setItem(
-    'cart',
-    JSON.stringify(updatedContent)
-  );
-
-  cartNum.textContent = getCartLength();
-}
-
-//! REMOVING ONE OF A KIND FROM CART
-function removeOneFromCart(item) {
-  const cartContent = JSON.parse(localStorage.getItem('cart'));
-
-  cartContent.splice(cartContent.indexOf(item), 1);
-
-  localStorage.setItem(
-    'cart',
-    JSON.stringify(cartContent)
-  );
-
-  cartNum.textContent = getCartLength();
-}
-
-//! EMPTYING CART
-function emptyCart() {
-  localStorage.setItem(
-    'cart',
-    JSON.stringify([])
-  );
-
-  cartNum.textContent = getCartLength();
-}
+const cart = new Cart();
